df1f75ee1d51833aa7756f691e7434498f09ec19
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-17 12:47:29 -0400

    Finished Exercise

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace csharp_web_dev_lsn9exceptions
 {
@@ -7,18 +8,38 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Cannot Divide by Zero");
+            }
+            return x / y;
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName.Equals("") || fileName.Equals(null))
+            {
+                throw new FormatException("The File / FileName is Empty");
+            }
+            else if (fileName.EndsWith(".cs"))
+            {
+                return 1;
+            }
+            return 0;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Console.WriteLine(Divide(42, 0));
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -27,7 +48,22 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> submission in students)
+            {
+                int grade = -1;
+                try
+                {
+                    grade = CheckFileExtension(submission.Value);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Student: " + submission.Key + ": " + e.Message);
+                }
+                if (grade == 0 || grade == 1)
+                {
+                    Console.WriteLine("Student: " + submission.Key + ": " + grade * 100 + "%");
+                }
+            }
         }
     }
 }
